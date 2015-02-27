45930bf0aff3d308af07ce056ade1a5fbff240f8
Takashi Kikuchi <tkikuchi36@gmail.com>
2015-02-27 22:41:04 +0900

    add hello world source by Java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+	‚Í‚¶‚ß‚Ä‚ÌJava
+
+	@author T.Kikuchi
+	@date 2015.2.27
+*/
+public class HelloWorld
+{
+	static public void main(String[] args)
+	{
+		System.out.println("Hello world!!");
+	}
+}
+
+
